8b68a9e644b54f85992d031d090f7e611db11662
Paul Planchon <paul@pdubs.xyz>
2017-12-11 17:33:18 +0100

    création des différentes const et types

--- gameOfLife.pas
+++ gameOfLife.pas
@@ -1,2 +1,23 @@
 PROGRAM gameOfLife;
-Uses Crt;
+USES Crt;
+
+CONST 
+	M    = 20;
+	N    = 20;
+	MORT = 0;
+	VIE  = 1;
+	
+// position d'une cellule
+TYPE typePosition = Record
+	x : integer;
+	y : integer;
+END;
+
+//tableau de position de cellules (pas la grille)
+TYPE tabPosition = array [0..M] of typePosition;
+	
+TYPE typeGrille  = array [0..N, 0..N] of INTEGER;
+
+BEGIN
+
+END.
